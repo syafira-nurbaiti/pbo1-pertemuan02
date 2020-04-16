1b6930b41ff60f1cd7e3d1782171b7bcc706a49f
Syafira-nurbaiti <cosmic947@gmail.com>
2020-04-16 12:06:45 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner =  new Scanner(System.in);
+  String nama;
+  int jumlahSks;
+  double IPK;
+
+  System.out.println("== Selamat Datang di Program Syafira Nurbaiti ==");
+
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
- }
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = scanner.nextInt();
+
+  System.out.print("Masukkan IPK sementara :");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.println("Total Nilai = "+jumlah);
+ 
+  System.out.println("== TERIMAKASIH ==");
+}
 }
