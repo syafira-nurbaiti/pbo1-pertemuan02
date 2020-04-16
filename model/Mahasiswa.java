59b605ae905b6150190eb4e4753d8a6e8398b229
Syafira-nurbaiti <cosmic947@gmail.com>
2020-04-16 11:55:57 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("===== Data Mahasiswa =====");
+  System.out.println("NPM  : 18630387");
+  System.out.println("Nama : Syafira Nurbaiti");
+  System.out.println("==========================");
+ }
+}
